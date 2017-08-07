adf091657e6134d37e385ac1ee950053b4734e98
Gihun Ham <progh2@gmail.com>
2017-08-08 05:22:09 +0900

    1. 계좌 조회 기능 구현하기 - 제대로 만들기

--- src/kr/hs/emirim/unittest/bankbusiness/main/Account.java
+++ src/kr/hs/emirim/unittest/bankbusiness/main/Account.java
@@ -1,12 +1,12 @@
 package kr.hs.emirim.unittest.bankbusiness.main;
 
 public class Account {
-
+	private int mMoney;
 	public Account(int money) {
-		
+		mMoney = money;
 	}
 
 	public int getBalance() {
-		return 10000;
+		return mMoney;
 	}	
 }
