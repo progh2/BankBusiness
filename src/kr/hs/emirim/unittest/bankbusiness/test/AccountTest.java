3a49bdc0d5f5b3c0005db2918529c03e0c234e28
Gihun Ham <progh2@gmail.com>
2017-08-08 05:32:43 +0900

    1. setUp 메소드 작성하기(@Before 사용)

--- src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
@@ -2,15 +2,22 @@ package kr.hs.emirim.unittest.bankbusiness.test;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import kr.hs.emirim.unittest.bankbusiness.main.Account;
 
 public class AccountTest {
 
+	Account account;
+	
+	@Before
+	public void setUp() {
+		account = new Account(10000);
+	}
+	
 	@Test
 	public void testCreateAccount() throws Exception {
-		Account account = new Account(10000);
 		if( account == null ) {
 			fail("계좌 생성 실패!");
 		}
@@ -18,7 +25,6 @@ public class AccountTest {
 	
 	@Test
 	public void testGetBalance() throws Exception {
-		Account account = new Account(10000);
 		if(account.getBalance() != 10000) {
 			fail("계좌 조회 실패!");
 		}
