a7f4c8debe319c90f1aa366073168e6e5b74cadd
Gihun Ham <progh2@gmail.com>
2017-08-08 04:55:26 +0900

    테스트 메소드 @Test 어노테이션 적용

--- src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
@@ -1,26 +1,16 @@
 package kr.hs.emirim.unittest.bankbusiness.test;
 
+import org.junit.Test;
+
 import kr.hs.emirim.unittest.bankbusiness.main.Account;
 
 public class AccountTest {
 
+	@Test
 	public void testCreateAccount() throws Exception {
 		Account account = new Account();
 		if( account == null ) {
 			throw new Exception("계좌 생성 실패!");
 		}
 	}
-	
-	public static void main(String[] args) {
-		AccountTest test = new AccountTest();
-		try {
-			test.testCreateAccount();
-		} catch (Exception e) {
-			System.out.println("실패(X)");
-			e.printStackTrace();
-			return;
-		}
-		System.out.println("성공(O)");
-	}
-
 }
