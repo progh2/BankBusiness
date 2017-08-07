540abd7d4db2ed242735f9f6b3f97bc76e6e43aa
Gihun Ham <progh2@gmail.com>
2017-08-08 01:21:21 +0900

    Account 객체개 생성되었는지 확인하는 테스트 메소드 추가

--- src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
@@ -2,6 +2,13 @@ package kr.hs.emirim.unittest.bankbusiness.test;
 
 public class AccountTest {
 
+	public void testCreateAccount() {
+		Account account = new Account();
+		if( account == null ) {
+			throw new Exception("계좌 생성 실패!");
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
