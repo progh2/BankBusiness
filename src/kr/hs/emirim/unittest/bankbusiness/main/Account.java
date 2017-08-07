708a09dabf7b18c8420fb0abb2d75a5dcf2aa376
Gihun Ham <progh2@gmail.com>
2017-08-08 01:54:07 +0900

    Account 객체 추가 및 구현

---
+++ src/kr/hs/emirim/unittest/bankbusiness/main/Account.java
@@ -0,0 +1,5 @@
+package kr.hs.emirim.unittest.bankbusiness.main;
+
+public class Account {
+
+}
--- src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/unittest/bankbusiness/test/AccountTest.java
@@ -1,8 +1,10 @@
 package kr.hs.emirim.unittest.bankbusiness.test;
 
+import kr.hs.emirim.unittest.bankbusiness.main.Account;
+
 public class AccountTest {
 
-	public void testCreateAccount() {
+	public void testCreateAccount() throws Exception {
 		Account account = new Account();
 		if( account == null ) {
 			throw new Exception("계좌 생성 실패!");
@@ -10,8 +12,15 @@ public class AccountTest {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AccountTest test = new AccountTest();
+		try {
+			test.testCreateAccount();
+		} catch (Exception e) {
+			System.out.println("실패(X)");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("성공(O)");
 	}
 
 }
